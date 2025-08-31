220671972e0fa40a19347f5e534dde17ca141971
vishv balar <vishvbalar@gmail.com>
2025-08-31 15:42:39 +0530

    Day-9 cart page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login.jsx'
 import AllProducts from './pages/AllProducts.jsx'
 import ProductCategory from './pages/ProductCategory.jsx'
 import ProductDetails from './pages/ProductDetails.jsx'
+import Cart from './pages/Cart.jsx'
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
           <Route  path='/products' element={<AllProducts/>}/>
           <Route  path='/products/:category' element={<ProductCategory/>}/>
           <Route  path='/products/:category/:id' element={<ProductDetails/>}/>
+          <Route  path='/cart' element={<Cart/>}/>
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
